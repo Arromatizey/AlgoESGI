92665db40cb08c3397cfc972b92a1a999a96feda
Arromatizey <92511895+Arromatizey@users.noreply.github.com>
2024-10-30 15:15:50 +0100

    Update Palindrome.c
    Code copier de TP00.c
    Pour plus de lisibilité

--- Palindrome.c
+++ Palindrome.c
@@ -17,6 +17,18 @@ bool estPalindrome(const char *mot) {
     return true;
 }
 
+
+//complexity O(n)
+void reverse_string(char str[]) {
+    int n = strlen(str);
+    for (int i = 0; i < n / 2; i++) {
+        char temp = str[i];
+        str[i] = str[n - i - 1];
+        str[n - i - 1] = temp;
+    }
+}
+
+
 int main() {
     char mot[100];
 
