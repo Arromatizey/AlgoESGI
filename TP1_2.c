959ae2f2e909fd28c6e8fbc06eab431a8e5d6ea1
Arromatizey <92511895+Arromatizey@users.noreply.github.com>
2024-10-30 12:14:50 +0100

    Add files via upload
    Sum Recursif

---
+++ TP1_2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int sum_recursive(int arr[], int n) {
+    if (n <= 0) {
+        return 0;
+    }
+    return arr[n - 1] + sum_recursive(arr, n - 1);
+}
+
+int main() {
+    int array[] = {1, 2, 3, 4, 5, 6, 7};
+    int size = sizeof(array) / sizeof(array[0]);
+    printf("Sum of elements: %d\n", sum_recursive(array, size));
+    return 0;
+}
