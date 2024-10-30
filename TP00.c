0bec70bb1b03cc4fe6da040e29c97e2856306a63
Arromatizey <92511895+Arromatizey@users.noreply.github.com>
2024-10-30 11:37:52 +0100

    Add files via upload

---
+++ TP00.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int find_max(int arr[], int n) {
+    int max_value = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max_value) {
+            max_value = arr[i];
+        }
+    }
+    return max_value;
+}
+
+int factorial(int n) {
+    int result = 1;
+    for (int i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+void reverse_string(char str[]) {
+    int n = strlen(str);
+    for (int i = 0; i < n / 2; i++) {
+        char temp = str[i];
+        str[i] = str[n - i - 1];
+        str[n - i - 1] = temp;
+    }
+}
+
+int main() {
+    char text[] = "hello";
+    reverse_string(text);
+    printf("Reversed string: %s\n", text);
+    return 0;
+}
